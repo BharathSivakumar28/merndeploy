247e0d8bf9a393533f0c30f011427d7ee333e6a6
bharath sivakumar <bharathsivakumar@bharaths-MacBook-Air.local>
2023-10-03 21:14:12 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 // connect MongoDB
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    const PORT = process.env.PORT || 8000
+mongoose.connect("mongodb+srv://root:root@cluster0.brxjgfq.mongodb.net/?retryWrites=true&w=majority").then(() => {
+    const PORT = 8000
     app.listen(PORT, () => {
         console.log(`App is Listening on PORT ${PORT}`);
     })
